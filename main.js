9d0666268774c2e2a74c9c00fcebd9d0ad0cadd8
Gencehan <gencehangurcam@gmail.com>
2021-11-05 14:18:06 +0100

    project init

---
+++ main.js
@@ -0,0 +1,18 @@
+content = document.querySelector('#content');
+console.log(content);
+
+content1 = document.querySelectorAll('#content');
+console.log(content1);
+
+content3 = document.querySelectorAll(".important")
+console.log(content3.innerHTML);
+
+
+// content3.forEach(element => {
+//     element.charAt(0).toUpperCase() + element.slice(1);
+// });
+
+content4 = document.querySelector('.grandParagraphe');
+console.log(content4);
+content5 = document.getElementsByClassName("grandParagraphe")
+console.log(content5);
